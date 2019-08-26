89d7cd82bd99cd310f67715e21c094ed000e3b16
Roman Zakrevskyi <romul3003@gmail.com>
2019-08-26 14:02:42 +0300

    Updates

---
+++ precache-manifest.001c4031bf6bf65129492f7e40aa5663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046ec128d0f11f05c962c2d030f076b1",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "51a8e43f961b12da7f88",
+    "url": "/meme-generator/static/css/main.64a78fc8.chunk.css"
+  },
+  {
+    "revision": "27364f44d23f8d1961ed",
+    "url": "/meme-generator/static/js/2.99533dc9.chunk.js"
+  },
+  {
+    "revision": "51a8e43f961b12da7f88",
+    "url": "/meme-generator/static/js/main.5530834a.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  }
+]);
